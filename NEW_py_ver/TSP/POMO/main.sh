43fdc6ba2261ca6a28f5d69ca771c137284efe35
orange984 <subaru.kimura.s4@dc.tohoku.ac.jp>
2022-01-24 13:22:46 +0900

    change main.sh

--- NEW_py_ver/TSP/POMO/main.sh
+++ NEW_py_ver/TSP/POMO/main.sh
@@ -4,7 +4,7 @@ for var in 20 50 100; do
   python test_n20.py  --problem_size $var\
                       --pomo_size $var\
                       --test_episodes 100000\
-                      --test_batch_size 1000\
+                      --test_batch_size 10000\
                       --augmentation_enable True\
                       --aug_factor 8\
                       --aug_batch_size 1000
