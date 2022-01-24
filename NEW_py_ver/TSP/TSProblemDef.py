7176a9997cd80f2c6b15ec9e07a5d775cfaaf1fe
orange984 <subaru.kimura.s4@dc.tohoku.ac.jp>
2022-01-24 12:59:18 +0900

    for augmentation

--- NEW_py_ver/TSP/TSProblemDef.py
+++ NEW_py_ver/TSP/TSProblemDef.py
@@ -1,12 +1,31 @@
 
 import torch
 import numpy as np
+import math
 
 
 def get_random_problems(batch_size, problem_size):
     problems = torch.rand(size=(batch_size, problem_size, 2))
     # problems.shape: (batch, problem, 2)
     return problems
+
+
+def get_rotation_matrix(rad):
+    rot = torch.tensor([[np.cos(rad), -np.sin(rad)],
+                        [np.sin(rad), np.cos(rad)]])
+    #print(type(rot))
+    return rot
+
+
+def rotate(xy, rad):
+    xy = xy - 0.5
+    batch = xy.shape[0]
+    problem = xy.shape[1]
+    xy = torch.reshape(xy, (-1, 2))
+    for i in range(xy.shape[0]):
+      xy[i] = torch.mv(get_rotation_matrix(rad).double(), (xy[i]).double())
+    xy = xy + 0.5
+    return torch.reshape(xy, (batch, problem, 2))
 
 
 def augment_xy_data_by_8_fold(problems):
@@ -17,15 +36,97 @@ def augment_xy_data_by_8_fold(problems):
     # x,y shape: (batch, problem, 1)
 
     dat1 = torch.cat((x, y), dim=2)
-    dat2 = torch.cat((1 - x, y), dim=2)
-    dat3 = torch.cat((x, 1 - y), dim=2)
-    dat4 = torch.cat((1 - x, 1 - y), dim=2)
+    dat2 = torch.cat(((1 - x), y), dim=2)
+    dat3 = torch.cat((x, (1 - y)), dim=2)
+    dat4 = torch.cat(((1 - x), (1 - y)), dim=2)
     dat5 = torch.cat((y, x), dim=2)
-    dat6 = torch.cat((1 - y, x), dim=2)
-    dat7 = torch.cat((y, 1 - x), dim=2)
-    dat8 = torch.cat((1 - y, 1 - x), dim=2)
+    dat6 = torch.cat(((1 - y), x), dim=2)
+    dat7 = torch.cat((y, (1 - x)), dim=2)
+    dat8 = torch.cat(((1 - y), (1 - x)), dim=2)
 
     aug_problems = torch.cat((dat1, dat2, dat3, dat4, dat5, dat6, dat7, dat8), dim=0)
     # shape: (8*batch, problem, 2)
+
+    return aug_problems
+
+
+def augment_xy_data_by_10_fold(problems):
+    # problems.shape: (batch, problem, 2)
+
+    x = problems[:, :, [0]]
+    y = problems[:, :, [1]]
+    # x,y shape: (batch, problem, 1)
+
+    dat1 = torch.cat((x, y), dim=2)
+    dat2 = torch.cat(((1 - x), y), dim=2)
+    dat3 = torch.cat((x, (1 - y)), dim=2)
+    dat4 = torch.cat(((1 - x), (1 - y)), dim=2)
+    dat5 = torch.cat((y, x), dim=2)
+    dat6 = torch.cat(((1 - y), x), dim=2)
+    dat7 = torch.cat((y, (1 - x)), dim=2)
+    dat8 = torch.cat(((1 - y), (1 - x)), dim=2)
+    dat9 = torch.cat((x+0.1, y+0.1), dim=2)
+    dat10 = torch.cat((x-0.1, y-0.1), dim=2)
+
+    aug_problems = torch.cat(
+        (dat1, dat2, dat3, dat4, dat5, dat6, dat7, dat8, dat9, dat10), dim=0)
+    # shape: (10*batch, problem, 2)
+
+    return aug_problems
+
+
+def augment_xy_data_by_12_fold(problems):
+    # problems.shape: (batch, problem, 2)
+
+    x = problems[:, :, [0]]
+    y = problems[:, :, [1]]
+    # x,y shape: (batch, problem, 1)
+
+    dat1 = torch.cat((x, y), dim=2)
+    dat2 = torch.cat(((1 - x), y), dim=2)
+    dat3 = torch.cat((x, (1 - y)), dim=2)
+    dat4 = torch.cat(((1 - x), (1 - y)), dim=2)
+    dat5 = torch.cat((y, x), dim=2)
+    dat6 = torch.cat(((1 - y), x), dim=2)
+    dat7 = torch.cat((y, (1 - x)), dim=2)
+    dat8 = torch.cat(((1 - y), (1 - x)), dim=2)
+    dat9 = torch.cat((x+0.1, y+0.1), dim=2)
+    dat10 = torch.cat((x-0.1, y-0.1), dim=2)
+    dat11 = torch.cat((x+0.1, y-0.1), dim=2)
+    dat12 = torch.cat((x-0.1, y+0.1), dim=2)
+    aug_problems = torch.cat(
+        (dat1, dat2, dat3, dat4, dat5, dat6, dat7, dat8, dat9, dat10, dat11, dat12), dim=0)
+    # shape: (12*batch, problem, 2)
+
+    return aug_problems
+
+
+def augment_xy_data_by_16_fold(problems):
+    # problems.shape: (batch, problem, 2)
+
+    x = problems[:, :, [0]]
+    y = problems[:, :, [1]]
+    # x,y shape: (batch, problem, 1)
+
+    dat1 = torch.cat((x, y), dim=2)
+    dat2 = torch.cat(((1 - x), y), dim=2)
+    dat3 = torch.cat((x, (1 - y)), dim=2)
+    dat4 = torch.cat(((1 - x), (1 - y)), dim=2)
+    dat5 = torch.cat((y, x), dim=2)
+    dat6 = torch.cat(((1 - y), x), dim=2)
+    dat7 = torch.cat((y, (1 - x)), dim=2)
+    dat8 = torch.cat(((1 - y), (1 - x)), dim=2)
+    dat9 = torch.cat((x+0.1, y+0.1), dim=2)
+    dat10 = torch.cat((x-0.1, y-0.1), dim=2)
+    dat11 = torch.cat((x+0.1, y-0.1), dim=2)
+    dat12 = torch.cat((x-0.1, y+0.1), dim=2)
+    dat13 = torch.cat((x+0.2, y+0.2), dim=2)
+    dat14 = torch.cat((x-0.2, y-0.2), dim=2)
+    dat15 = torch.cat((x+0.2, y-0.2), dim=2)
+    dat16 = torch.cat((x-0.2, y+0.2), dim=2)
+
+    aug_problems = torch.cat(
+        (dat1, dat2, dat3, dat4, dat5, dat6, dat7, dat8, dat9, dat10, dat11, dat12, dat13, dat14, dat15, dat16), dim=0)
+    # shape: (16*batch, problem, 2)
 
     return aug_problems
